ab991ff7ec4ffa5cca4739b97ceff2909b20eaaa
mosqu1t0 <mosquito@email.cn>
2022-05-31 22:26:54 +0800

    katex

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,11 @@ export default {
     title: "Mosquito's Note",
     description: '个人笔记',
     lastUpdated: true,
-    head: [['link', { rel: 'icon', href: '/img/logo.png '}]],
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.png '}],
+        ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'}],
+        ['link', { rel: "stylesheet", href: "https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css"}]
+    ],
     locales: [],
     themeConfig: {
 
@@ -103,11 +107,13 @@ export default {
 
             ]
 
-    },
+        }
 
+    },
     markdown: {
         lineNumbers: true,
+        config: md => {
+            md.use(require('markdown-it-katex'))   
+        }
     },
-
-    }
 }
